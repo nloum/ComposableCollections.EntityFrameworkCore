78a1edfb7ddf27559fb014e9fadcf4ffb1abb396
Nate Loum <39776302+nloum@users.noreply.github.com>
2020-08-25 10:22:43 -0500

    Add unit test project

---
+++ src/ComposableCollections.EntityFrameworkCore.Tests/DatabaseLayerTests.cs
@@ -0,0 +1,194 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reactive.Subjects;
+using AutoMapper;
+using LiveLinq.Dictionary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UtilityDisposables;
+
+namespace ComposableCollections.EntityFrameworkCore.Tests
+{
+    public class WorkItemDto
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public PersonDto AssignedTo { get; set; }
+        public Guid? AssignedToForeignKey { get; set; }
+    }
+
+    public class PersonDto
+    {
+        public PersonDto()
+        {
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<WorkItemDto> AssignedWorkItems { get; set; }
+    }
+    
+    public class WorkItem {
+        public WorkItem(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; }
+        public string Description { get; set; }
+        public Person AssignedTo { get; set; }
+    }
+
+    public class Person
+    {
+        public Person(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; }
+        public string Name { get; set; }
+        public ICollection<WorkItem> AssignedWorkItems { get; set; }
+    }
+
+    public class MyDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PersonDto>()
+                .HasMany(x => x.AssignedWorkItems)
+                .WithOne(x => x.AssignedTo)
+                .HasForeignKey(x => x.AssignedToForeignKey);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=tasks.db");
+        }
+        
+        public DbSet<WorkItemDto> WorkItem { get; set; }
+        public DbSet<PersonDto> Person { get; set; }
+    }
+
+    public static class Transaction
+    {
+        public static Transaction<TPeople, TTasks> Create<TPeople, TTasks>(TPeople people, TTasks tasks, IDisposable disposable)
+        {
+            return new Transaction<TPeople, TTasks>(people, tasks, disposable);
+        }
+    }
+    
+    public class Transaction<TPeople, TTasks> : IDisposable
+    {
+        private readonly IDisposable _disposable;
+
+        public Transaction(TPeople people, TTasks tasks, IDisposable disposable)
+        {
+            _disposable = disposable;
+            People = people;
+            Tasks = tasks;
+        }
+
+        public TPeople People { get; }
+        public TTasks Tasks { get; }
+
+        public void Dispose()
+        {
+            _disposable.Dispose();
+        }
+    }
+
+    [TestClass]
+    public class DatabaseLayerTests
+    {
+        [TestMethod]
+        public void ShouldHandleManyToOneRelationships()
+        {
+            if (File.Exists("tasks.db"))
+            {
+                File.Delete("tasks.db");
+            }
+            
+            var preserveReferencesState = new PreserveReferencesState();
+            
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<WorkItem, WorkItemDto>()
+                    .ConstructUsing(preserveReferencesState)
+                    .ReverseMap()
+                    .ConstructUsing(preserveReferencesState, dto => new WorkItem(dto.Id));
+
+                cfg.CreateMap<Person, PersonDto>()
+                    .ConstructUsing(preserveReferencesState)
+                    .ReverseMap()
+                    .ConstructUsing(preserveReferencesState, dto => new Person(dto.Id));
+            });
+
+            var mapper = mapperConfig.CreateMapper();
+            
+            var peopleChanges = new Subject<IDictionaryChangeStrict<Guid, Person>>();
+            var taskChanges = new Subject<IDictionaryChangeStrict<Guid, WorkItem>>();
+
+            var start = TransactionalDatabase.Create(() => new MyDbContext(), x => x.Database.Migrate());
+            var infrastructure = start.Select(
+                dbContext =>
+                {
+                    var tasks = dbContext.AsComposableReadOnlyDictionary(x => x.WorkItem, x => x.Id)
+                        .WithMapping<Guid, WorkItem, WorkItemDto>(mapper)
+                        .WithLiveLinq(taskChanges)
+                        .WithBuiltInKey(t => t.Id);
+                    var people = dbContext.AsComposableReadOnlyDictionary(x => x.Person, x => x.Id)
+                        .WithMapping<Guid, Person, PersonDto>(mapper)
+                        .WithLiveLinq(peopleChanges)
+                        .WithBuiltInKey(p => p.Id);
+                    return Transaction.Create(people, tasks, dbContext);
+                },
+                dbContext =>
+                {
+                    var tasks = dbContext.AsComposableDictionary(x => x.WorkItem, x => x.Id)
+                        .WithMapping<Guid, WorkItem, WorkItemDto>(mapper)
+                        .WithLiveLinq(taskChanges)
+                        .WithBuiltInKey(t => t.Id);
+                    var people = dbContext.AsComposableDictionary(x => x.Person, x => x.Id)
+                        .WithMapping<Guid, Person, PersonDto>(mapper)
+                        .WithLiveLinq(peopleChanges)
+                        .WithBuiltInKey(p => p.Id);
+                    return Transaction.Create(people, tasks, new AnonymousDisposable(() =>
+                    {
+                        dbContext.SaveChanges();
+                        dbContext.Dispose();
+                    }));
+                });
+
+            var joeId = Guid.NewGuid();
+            var taskId = Guid.NewGuid();
+            
+            using (var transaction = infrastructure.BeginWrite())
+            {
+                var joe = new Person(joeId)
+                {
+                    Name = "Joe"
+                };
+
+                transaction.People.Add(joe);
+            
+                var washTheCar = new WorkItem(taskId)
+                {
+                    Description = "Wash the car",
+                    AssignedTo = joe
+                };
+            
+                transaction.Tasks.Add(washTheCar);   
+            }
+
+            using (var transaction = infrastructure.BeginWrite())
+            {
+                var joe = transaction.People[joeId];
+                var washTheCar = transaction.Tasks[taskId];
+            }
+        }
+    }
+}
---
+++ src/ComposableCollections.EntityFrameworkCore.Tests/Migrations/20200821115110_InitialCreate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ComposableCollections.EntityFrameworkCore.Tests.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkItem",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    AssignedToForeignKey = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WorkItem_Person_AssignedToForeignKey",
+                        column: x => x.AssignedToForeignKey,
+                        principalTable: "Person",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkItem_AssignedToForeignKey",
+                table: "WorkItem",
+                column: "AssignedToForeignKey");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WorkItem");
+
+            migrationBuilder.DropTable(
+                name: "Person");
+        }
+    }
+}
